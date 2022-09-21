7c1ef2f5536aa2ad96f661592b388c1dfb0154ba
Cyber-Var <renata.kim.03@mail.ru>
2022-09-21 16:08:18 +0600

    GUI improved. Version for MacOS ready.

--- GUI.py
+++ GUI.py
@@ -1,12 +1,13 @@
 import tkinter as tk
 from tkinter import *
 import tkinter.ttk as ttk
+# for mac
+from tkmacosx import Button
 
 import requests
 import json
 from datetime import date
 from datetime import timedelta
-
 
 all_currencies = {
     "AUD": "Australian dollar",
@@ -16,7 +17,8 @@ all_currencies = {
     "EUR": "Euro",
     "GBP": "Pound sterling",
     "HKD": "Hong Kong dollar", "HRK": "Croatian kuna", "HUF": "Hungarian forint",
-    "IDR": "Indonesian rupiah", "ILS": "Israeli new shekel", "INR": "Indian rupee", "ISK": "Icelandic krona", "I44": "Import-weighted krone exchange rate",
+    "IDR": "Indonesian rupiah", "ILS": "Israeli new shekel", "INR": "Indian rupee", "ISK": "Icelandic krona",
+    "I44": "Import-weighted krone exchange rate",
     "JPY": "Japanese yen",
     "KRW": "South Korean won",
     "MMK": "Myanmar kyat", "MYR": "Malaysian ringgit", "MXN": "Mexican peso",
@@ -43,7 +45,10 @@ def is_float(number):
 
 
 root = tk.Tk()
-root.geometry("300x450")
+# for mac:
+root.geometry("270x315")
+# root.geometry("270x317")
+root.configure(background='#B4CFEC')
 root.title("Currency Converter")
 
 
@@ -57,7 +62,7 @@ class GUI:
         for cur in all_currencies:
             values.append(cur + " (" + all_currencies[cur] + ")")
 
-        self.box_cur1 = ttk.Combobox(root, values=values, width=27, font="Verdana 20 bold")
+        self.box_cur1 = ttk.Combobox(root, values=values, width=27)
         self.box_cur2 = ttk.Combobox(root, values=values, width=27)
 
         self.draw()
@@ -66,13 +71,17 @@ class GUI:
 
         # Top frame:
 
-        frame_entry = Frame(root)
+        frame_entry = Frame(root, bg='#B4CFEC')
         frame_entry.grid()
 
-        self.txt_amount = Entry(frame_entry, text="Enter amount: ", font=("Comic Sans MS", 29, "bold"), width=9)
-        self.txt_amount.grid(row=0, column=0, sticky="WE", padx=0)
+        self.txt_amount = Entry(frame_entry, font=("Comic Sans MS", 20, "bold"), width=12,
+                                highlightbackground='#B4CFEC')
+        self.txt_amount.grid(row=0, column=0, sticky="WE", padx=0, ipady=7)
 
-        btn_enter = Button(frame_entry, text="Enter", width=10, height=3, command=self.entered)
+        # for mac:
+        btn_enter = Button(frame_entry, text="Enter", width=100, height=50, bg='#595959', fg='white',
+                           highlightbackground='#B4CFEC', command=self.entered)
+        # btn_enter = Button(frame_entry, text="Enter", width=10, height=3, bg="#7393B3", command=self.entered)
         btn_enter.grid(row=0, column=1, sticky="WE", padx=0)
 
         root.grid_columnconfigure(0, weight=1)
@@ -88,36 +97,68 @@ class GUI:
 
         # Numbers frame:
 
+        s = ttk.Style()
+        s.configure('My.TFrame', background='red')
+
         frame_numbers = Frame(root)
         frame_numbers.grid()
 
-        btn_1 = Button(frame_numbers, text="1", command=lambda: self.number_clicked("1"))
-        btn_2 = Button(frame_numbers, text="2", command=lambda: self.number_clicked("2"))
-        btn_3 = Button(frame_numbers, text="3", command=lambda: self.number_clicked("3"))
+        # for mac:
+        btn_1 = Button(frame_numbers, text="1", width=89, height=50, bg='#595959', fg='white',
+                       highlightbackground='#B4CFEC', command=lambda: self.number_clicked("1"))
+        btn_2 = Button(frame_numbers, text="2", width=89, height=50, bg='#595959', fg='white',
+                       highlightbackground='#B4CFEC', command=lambda: self.number_clicked("2"))
+        btn_3 = Button(frame_numbers, text="3", width=89, height=50, bg='#595959', fg='white',
+                       highlightbackground='#B4CFEC', command=lambda: self.number_clicked("3"))
+
+        btn_4 = Button(frame_numbers, text="4", width=89, height=50, bg='#595959', fg='white',
+                       highlightbackground='#B4CFEC', command=lambda: self.number_clicked("4"))
+        btn_5 = Button(frame_numbers, text="5", width=89, height=50, bg='#595959', fg='white',
+                       highlightbackground='#B4CFEC', command=lambda: self.number_clicked("5"))
+        btn_6 = Button(frame_numbers, text="6", width=89, height=50, bg='#595959', fg='white',
+                       highlightbackground='#B4CFEC', command=lambda: self.number_clicked("6"))
+
+        btn_7 = Button(frame_numbers, text="7", width=89, height=50, bg='#595959', fg='white',
+                       highlightbackground='#B4CFEC', command=lambda: self.number_clicked("7"))
+        btn_8 = Button(frame_numbers, text="8", width=89, height=50, bg='#595959', fg='white',
+                       highlightbackground='#B4CFEC', command=lambda: self.number_clicked("8"))
+        btn_9 = Button(frame_numbers, text="9", width=89, height=50, bg='#595959', fg='white',
+                       highlightbackground='#B4CFEC', command=lambda: self.number_clicked("9"))
+
+        btn_dot = Button(frame_numbers, text=".", width=89, height=50, bg='#595959', fg='white',
+                         highlightbackground='#B4CFEC', command=lambda: self.number_clicked("."))
+        btn_0 = Button(frame_numbers, text="0", width=89, height=50, bg='#595959', fg='white',
+                       highlightbackground='#B4CFEC', command=lambda: self.number_clicked("0"))
+        btn_back = Button(frame_numbers, text="⌫", width=89, height=50, bg='#595959', fg='white',
+                          highlightbackground='#B4CFEC', command=lambda: self.number_clicked("B"))
+
+        ''' btn_1 = Button(frame_numbers, text="1", width=9, height=3, bg="#7393B3", command=lambda: self.number_clicked("1"))
+        btn_2 = Button(frame_numbers, text="2", width=10, height=3, command=lambda: self.number_clicked("2"))
+        btn_3 = Button(frame_numbers, text="3", width=9, height=3, command=lambda: self.number_clicked("3"))
+
+        btn_4 = Button(frame_numbers, text="4", width=9, height=3, command=lambda: self.number_clicked("4"))
+        btn_5 = Button(frame_numbers, text="5", width=10, height=3, command=lambda: self.number_clicked("5"))
+        btn_6 = Button(frame_numbers, text="6", width=9, height=3, command=lambda: self.number_clicked("6"))
+
+        btn_7 = Button(frame_numbers, text="7", width=9, height=3, command=lambda: self.number_clicked("7"))
+        btn_8 = Button(frame_numbers, text="8", width=10, height=3, command=lambda: self.number_clicked("8"))
+        btn_9 = Button(frame_numbers, text="9", width=9, height=3, command=lambda: self.number_clicked("9"))
+
+        btn_dot = Button(frame_numbers, text=".", width=9, height=3, command=lambda: self.number_clicked("."))
+        btn_0 = Button(frame_numbers, text="0", width=10, height=3, command=lambda: self.number_clicked("0"))
+        btn_back = Button(frame_numbers, text="⌫", width=9, height=3, command=lambda: self.number_clicked("B")) '''
 
         btn_1.grid(row=0, column=0)
         btn_2.grid(row=0, column=1)
         btn_3.grid(row=0, column=2)
 
-        btn_4 = Button(frame_numbers, text="4", command=lambda: self.number_clicked("4"))
-        btn_5 = Button(frame_numbers, text="5", command=lambda: self.number_clicked("5"))
-        btn_6 = Button(frame_numbers, text="6", command=lambda: self.number_clicked("6"))
-
         btn_4.grid(row=1, column=0)
         btn_5.grid(row=1, column=1)
         btn_6.grid(row=1, column=2)
 
-        btn_7 = Button(frame_numbers, text="7", command=lambda: self.number_clicked("7"))
-        btn_8 = Button(frame_numbers, text="8", command=lambda: self.number_clicked("8"))
-        btn_9 = Button(frame_numbers, text="9", command=lambda: self.number_clicked("9"))
-
         btn_7.grid(row=2, column=0)
         btn_8.grid(row=2, column=1)
         btn_9.grid(row=2, column=2)
-
-        btn_dot = Button(frame_numbers, text=".", command=lambda: self.number_clicked("."))
-        btn_0 = Button(frame_numbers, text="0", command=lambda: self.number_clicked("0"))
-        btn_back = Button(frame_numbers, text="⌫", command=lambda: self.number_clicked("B"))
 
         btn_dot.grid(row=3, column=0)
         btn_0.grid(row=3, column=1)
@@ -160,9 +201,8 @@ class GUI:
         if result == -1:
             self.txt_amount.delete(0, END)
         else:
-            self.lbl_result.grid_forget()
-            self.lbl_result = Label(root, text="Result = " + str(result))
-            self.lbl_result.grid()
+            self.txt_amount.delete(0, END)
+            self.txt_amount.insert(0, str(result) + " " + cur_2)
 
     def calculate(self, rate1, rate2):
         if is_float(self.txt_amount.get()):
@@ -175,7 +215,7 @@ class GUI:
         if number in str_digits or number == "." and "." not in self.txt_amount.get():
             self.txt_amount.insert(len(self.txt_amount.get()), number)
         elif number == "B":
-            self.txt_amount.delete(len(self.txt_amount.get())-1, END)
+            self.txt_amount.delete(len(self.txt_amount.get()) - 1, END)
+
 
 GUI()
-
